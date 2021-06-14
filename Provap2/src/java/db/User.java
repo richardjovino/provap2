53c48ba0dde1b1dd2b35aed7fb57ba1c27b40398
palomaierardi <palomaierardi@hotmail.com>
2021-06-14 13:41:08 -0300

    criação dblistener e user

--- Provap2/src/java/db/User.java
+++ Provap2/src/java/db/User.java
@@ -5,10 +5,121 @@
  */
 package db;
 
+import java.util.ArrayList;
+import java.sql.*;
+import web.DbListener;
+
 /**
  *
  * @author palom
  */
 public class User {
+    private String name;
+    private String login;
+    private String role;
+    
+    public static ArrayList<User> getUsers() throws Exception{
+        ArrayList<User> list = new ArrayList<>();
+        Connection con = DbListener.getConnection();
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * from users");
+        while(rs.next()){
+            list.add(new User(
+                    rs.getString("name")
+                    , rs.getString("login")
+                    , rs.getString("role")
+            ));
+        }
+        rs.close();
+        stmt.close();
+        con.close();
+        return list;
+    }
+    
+    public static User getUser(String login, String pass) throws Exception{
+        User user = null;
+        Connection con = DbListener.getConnection();
+        String sql = "SELECT * from users WHERE login=? and password_hash=?";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        stmt.setString(1, login);
+        stmt.setLong(2, pass.hashCode());
+        ResultSet rs = stmt.executeQuery();
+        if(rs.next()){
+            user = new User(
+                    rs.getString("name")
+                    , rs.getString("login")
+                    , rs.getString("role")
+            );
+        }
+        rs.close();
+        stmt.close();
+        con.close();
+        return user;
+    }
+    
+    public static void addUser(String login, String name, String role, String password) throws Exception{
+        Connection con = DbListener.getConnection();
+        String sql = "INSERT INTO users(login, name, role, password_hash)"
+                + "VALUES(?, ?, ?, ?)";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        stmt.setString(1, login);
+        stmt.setString(2, name);
+        stmt.setString(3, role);
+        stmt.setLong(4, password.hashCode());
+        stmt.execute();
+        stmt.close();
+        con.close();
+    }
+    
+    public static void removeUser(String login) throws Exception{
+        Connection con = DbListener.getConnection();
+        String sql = "DELETE FROM users WHERE login = ?";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        stmt.setString(1, login);
+        stmt.execute();
+        stmt.close();
+        con.close();
+    }
+    
+    public static void changePassword(String login, String newPassword) throws Exception{
+        Connection con = DbListener.getConnection();
+        String sql = "UPDATE users SET password_hash=? WHERE login = ?";
+        PreparedStatement stmt = con.prepareStatement(sql);
+        stmt.setLong(1, newPassword.hashCode());
+        stmt.setString(2, login);
+        stmt.execute();
+        stmt.close();
+        con.close();
+    }
+    
+    public User(String name, String login, String role) {
+        this.name = name;
+        this.login = login;
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
 }
--- Provap2/src/java/web/DbListener.java
+++ Provap2/src/java/web/DbListener.java
@@ -5,10 +5,63 @@
  */
 package web;
 
+import db.User;
+import java.sql.*;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
 /**
  * Web application lifecycle listener.
  *
  * @author palom
  */
-public class DbListener {
+public class DbListener implements ServletContextListener {
+    public static final String CLASS_NAME = "org.sqlite.JDBC";
+    public static final String URL = "jdbc:sqlite:myfin.db";
+    
+    public static String step = null;
+    public static Exception exception = null;
+    
+    public static Connection getConnection() throws Exception{
+        return DriverManager.getConnection(URL);
+    }
+    
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try{
+            step = "Conectando com a base";
+            Class.forName(CLASS_NAME);
+            Connection con = getConnection();
+            Statement stmt = con.createStatement();
+            //stmt.execute("DROP TABLE users");
+            step = "Criando tabela de usuários";
+            String sql = "CREATE TABLE IF NOT EXISTS users("
+                    + "name VARCHAR(200) NOT NULL,"
+                    + "login VARCHAR(50) UNIQUE NOT NULL,"
+                    + "password_hash LONG,"
+                    + "role VARCHAR(20) NOT NULL"
+                    + ")";
+            stmt.execute(sql);
+            if(User.getUsers().isEmpty()){
+                step = "Inserindo usuário administrativo";
+                sql = "INSERT INTO users(name, login, password_hash, role) "
+                    + "VALUES('Administrador', 'admin', '"+("1234".hashCode())+"', 'ADMIN')";
+                stmt.execute(sql);
+                sql = "INSERT INTO users(name, login, password_hash, role) "
+                    + "VALUES('Paloma Ierardi', 'paloma', '"+("1234".hashCode())+"', 'USER')";
+                stmt.execute(sql); 
+            }
+                
+            
+            //TODO: implementar código de criação das outras tabelas
+            stmt.close();
+            con.close();
+        }catch(Exception ex){
+            exception = ex;
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
 }
